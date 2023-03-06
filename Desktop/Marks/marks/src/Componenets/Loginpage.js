bb4d00546053161e3ebdb0e4082553857aa0d8ab
bhuvana atmakuri <2100030055@kluniversity.in>
2023-03-06 22:57:32 +0530

    Mongodb

--- Desktop/Marks/marks/src/Componenets/Loginpage.js
+++ Desktop/Marks/marks/src/Componenets/Loginpage.js
@@ -1,31 +1,116 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+// import { useState } from 'react';
+ import axios from 'axios';
+
 export default function Loginpage() {
      const navigate=useNavigate()
+     //const [res,setRes] = useState('');
+    //  const [formData, setFormData] = useState({
+    //     userid: '',
+    //     pass: ''
+    //   });
+    //const [formData, setFormData] = useState(null);
+     //let re=" ";
+     const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+       
+        //  setFormData({
+        //     userid: data.get('userid'),
+        //     pass: data.get('pass')
+        //   });
+          console.log(data.get('userid'))
+        axios.post("http://localhost:1111/data",{
+            userid: data.get('userid'),
+            pass: data.get('pass'),
+            admin:data.get('admin')
+          }).then((result)=>{
+             console.log(result.data);
+             //setRes(result.data);
+             //console.log(res);
+             if(result.data==="True" ){
+                    console.log('Hai');
+                    navigate('Home')          
+             }
+             else if(result.data==="Tr"){
+              navigate('regis')
+             }
+             else{
+                 document.getElementById("piggy").innerHTML="Invalid Credentials!";
+             }
+
+         }).catch((error)=>{
+             console.log(error);
+         })    
+       }
+       // set up text to print, each item in array is new line
+
+     
   return (
     <>
-    <center><b><h1 className="hoo">Grade Check</h1></b></center>
     
-    <div className="form-bg">
+   
+   
+<section class="">
+ 
+  <div class="py-4 text-center text-lg-start" >
+    <div class="container">
+      <div class="row gx-lg-5 align-items-center">
+        <div class="col-lg-6 mb-5 mb-lg-0 split left">
+          <h1 class="my-1 display-3 k">
+           <center className="hoo"> Grade Check</center>
+            {/* <span class="text-primary">for your business</span> */}
+          </h1>
+          <br/>
+          <div className='css-typing'>
+          <p class="my-2">
+ Grade Check  is  a  marks management  system , that typically 
+</p>
+<p>
+ consists of two roles - the faculty and the student.The faculty role
+</p>
+
+
+<p>
+ involves entering the marks for various assessments and assigning 
+</p>
+<p>grades. The student role involves logging into the system and  </p>
+<p>
+checking their marks for various assessments, assignments,and exams.
+</p>
+<p> The system also allows students to track their progress over time</p>
+<p>and identify areas where they need to improve.It provides real-time  </p>
+<p>access to marks and grades, reducing the time and effort required</p>
+
+<p>  for manual record-keeping. Additionally, the system ensures the</p> <p>accuracy and security of data, preventing unauthorized access and  </p><p> tampering of marks.</p><p></p>
+
+    </div>
+        </div>
+<div className="spi">
+    <center><b><h1 className="hoo spli"> </h1></b>
+    <h3 id="piggy" className="hoo spl"> </h3></center>
+        <div className="form-bg ">
+        
     <div className="container">
-        <div className="row">
+   
+        <div className="row sp right">
             
                 <div className="form-container">
                     <h3 className="title">My Account</h3>
-                    <form className="form-horizontal">
+                    <form className="form-horizontal"  onSubmit={handleSubmit}>
+                        
                         <div className="form-icon">
                             <i className="fa fa-user-circle"></i>
                         </div>
                         <div className="form-group">
                             <span className="input-icon"><i class="fa fa-user"></i></span>
-                            <input type="email" className="form-control" placeholder="Username(Id Number)"/>
+                            <input type="number" className="form-control" name="userid" placeholder="Username(Id Number)"/>
                         </div>
                         <div className="form-group">
                             <span className="input-icon"><i className="fa fa-lock"></i></span>
-                            <input type="password" className="form-control" placeholder="Password"/>
-                            <h4>
-                            
-                           
+                            <input type="password" name="pass" className="form-control" placeholder="Password"/>
+                            <h4>   
                           {/* <div>
                         
                          <input type="radio" id="contactChoice1" name="contact" value="email" />
@@ -45,7 +130,8 @@ export default function Loginpage() {
                             </select>  */}
                             </h4>
                         </div>
-                        <button onClick={()=>navigate('Home')} className="btn signin">Login</button>
+                        <input type="submit" className="form-control form-control-lg log" value="Login"/>
+                        {/* <button onClick={()=>navigate('Home')} className="btn signin">Login</button> */}
                         <div>
                         <h6>Do not have an account?<a href="register">Sign up</a></h6>
                         </div>
@@ -54,7 +140,12 @@ export default function Loginpage() {
             </div>
         </div>
     </div>
-
+    </div>
+      </div>
+    </div>
+  </div>
+ 
+</section>
 
     </>
   )
--- Desktop/Marks/marks/src/Componenets/Registrationform.js
+++ Desktop/Marks/marks/src/Componenets/Registrationform.js
@@ -1,7 +1,152 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+
+ import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import axios from 'axios';
+import  { useRef } from "react";
+import emailjs from "@emailjs/browser";
 export default function Registrationform() {
-  const navigate=useNavigate()
+  
+  const form = useRef();
+   const navigate=useNavigate()
+  const [res,setRes] = useState(null);
+  
+  console.log(res);
+  // validateEmail = () => {
+  //   const email = this.refs.email.value;
+
+  //   if(email.includes('@gmail.com')){
+  //     document.getElementById("email").style.border = "2px solid green";
+  //     document.getElementById("erroremail").style.color = "green";
+  //     document.getElementById("erroremail").innerHTML="Email is of required format";
+  //     return true;
+  //   } else {
+  //     document.getElementById("email").style.border = "2px solid red";
+  //     document.getElementById("erroremail").style.color = "red";
+  //     document.getElementById("email").value="";
+  //     document.getElementById("erroremail").innerHTML="The email should be of the form example@gmail.com";
+  //     return false;
+  //   }}
+
+  //Mongodb------
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const data = new FormData(event.currentTarget);
+    let pa=data.get('pass');
+    let em=data.get('email');
+    let k;
+    if(em.includes('@gmail.com')){
+      k="True";
+    }
+    else{
+      k="False";
+    }
+    let patt = new RegExp("[0-9]");
+    let result = patt.test(pa);
+    let pa1 = data.get('userid');
+    axios.post("http://localhost:1111/re",{
+     
+    userid : data.get('userid'),
+
+  }).then((response) => {
+    console.log(response.data);
+   if (response.data === "True") {
+      document.getElementById("ii").style.border = "2px solid red";
+      document.getElementById("errorid").style.color = "red";
+      document.getElementById("errorid").innerHTML = "This Id number is already taken";
+      document.getElementById("ii").value = "";
+    }
+  
+   else if (pa1.length !== 10) {
+    document.getElementById("ii").style.border = "2px solid red";
+    document.getElementById("errorid").style.color = "red";
+    document.getElementById("errorid").innerHTML = "The id number should be of length 10";
+    document.getElementById("ii").value = "";
+   }
+  
+   axios.post("http://localhost:1111/r",{
+     
+   email : data.get('email'),
+
+ }).then((response) => {
+  console.log(response.data);
+  if (response.data === "True") {
+     document.getElementById("ema").style.border = "2px solid red";
+     document.getElementById("erroremail").style.color = "red";
+     document.getElementById("erroremail").innerHTML = "This email is already taken";
+     document.getElementById("ema").value = "";
+   }
+ 
+   else if(k==="False"){
+    document.getElementById("ema").style.border = "2px solid red";
+    document.getElementById("erroremail").style.color = "red";
+     document.getElementById("ema").value="";
+    document.getElementById("erroremail").innerHTML="The email should be of the form example@gmail.com";
+
+   }
+   else if(result===false || pa.length<5 || pa.length>20){
+        document.getElementById("bd").style.border = "2px solid red";
+         document.getElementById("errorpassword").style.color = "red";
+        var errorpassword1= "Password Must Contain atleast 1 digit and should be of length 5 to 20 ";
+        document.getElementById("bd").value="";
+        document.getElementById("errorpassword").innerHTML=errorpassword1;
+    }
+  
+    
+   else{
+    axios.post("http://localhost:1111/register",{
+      name : data.get('name'),
+      userid : data.get('userid'),
+      pass : data.get('pass'),
+      inlineRadioOptions: data.get('inlineRadioOptions'),
+      email:data.get('email'),
+      study:data.get('study'),
+      branch:data.get('branch'),
+      mand:data.get('mand'),
+      admin:'False'
+
+    }).then((response) => {
+      console.log(response.data);
+      setRes(response.data);
+      navigate('/')
+      
+    }).catch((error) => {
+        console.log(error);
+    })
+  }
+  }).catch((error) => {
+      console.log(error);
+  })
+  }).catch((error) => {
+  console.log(error);
+  })
+  
+
+   
+    //email sending
+    emailjs.sendForm(
+      "service_nsjmlm9",
+      "template_5xh67by",
+      form.current,
+      "tdcsrPVxRWEBjvfoA"
+    )
+    .then(
+      (result) => {
+        console.log(result.text);
+        console.log("message sent");
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+    
+
+  }
+  
+  
+  
   return (
     <>
    <center><h1 className='hoo'>Grade Check</h1></center> 
@@ -14,21 +159,21 @@ export default function Registrationform() {
           <div className="card-body ">
           <center  ><h2 className='hoo'>Registration Form</h2></center>
           <br/>
-            <form>
+            <form ref={form} onSubmit={handleSubmit}  >
 
               <div className="row">
                 <div className="col-md-6 mb-4">
 
                   <div className="form-outline">
-                    <input type="text" id="firstName" className="form-control form-control-lg" placeholder=' Name'/>
-                   
+                    <input type="text" id="firstName" name="name" className="form-control form-control-lg" placeholder=' Name' required/>
                   </div>
 
                 </div>
                 <div className="col-md-6 mb-4">
 
                   <div className="form-outline">
-                    <input type="text" id="lastName" className="form-control form-control-lg" placeholder='Id Number'/>
+                    <input type="number" id="ii" name="userid" className="form-control form-control-lg" placeholder='Id Number' required/>
+                    <p id="errorid"></p>
                   
                   </div>
 
@@ -39,8 +184,9 @@ export default function Registrationform() {
                 <div className="col-md-6 mb-4 d-flex align-items-center">
 
                   <div className="form-outline datepicker w-100">
-                    <input type="text" className="form-control form-control-lg" id="birthdayDate" placeholder='Password'/>
-                   
+                    <input type="password" className="form-control form-control-lg" name="pass" id="bd" placeholder='Password' required/>
+                    
+                    <p id="errorpassword"></p>
                   </div>
 
                 </div>
@@ -50,7 +196,7 @@ export default function Registrationform() {
 
                   <div className="form-check form-check-inline">
                     <input className="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                      value="option1" />
+                      value="option1" required/>
                     <label className="form-check-label" for="femaleGender">Female</label>
                   </div>
 
@@ -73,23 +219,24 @@ export default function Registrationform() {
                 <div className="col-md-6 mb-4 pb-2">
 
                   <div className="form-outline">
-                    <input type="email" id="emailAddress" className="form-control form-control-lg" placeholder='Email' />
-                    
+                    <input type="email" id="ema" name="email" className="form-control form-control-lg" placeholder='Email' required />
+                    <p id="erroremail"></p>
                   </div>
 
                 </div>
                 <div className="col-md-6 mb-4 pb-2">
 
                   <div className="form-outline">
-                  <select className="select form-control-lg">
-                    <option value="1" selected>Select year of study</option>
+                  <select className="select form-control-lg" placeholder="select" name="study" required>
+                    <option value="1"  disabled>Select year of study</option>
                     <option value="2">1st year</option>
                     <option value="3">2nd year</option>
                     <option value="4">3rd year</option>
                     <option value="4">4th year</option>
                   </select>
+                 
                   </div>
-
+                  (select your year of study)
                 </div>
               </div>
 
@@ -97,8 +244,8 @@ export default function Registrationform() {
               <div className="col-md-6 mb-4 pb-2">
 
 <div className="form-outline">
-<select className="select form-control-lg">
-                    <option value="1" selected>Select Branch</option>
+<select className="select form-control-lg" name="branch" required>
+                    <option value="1"  disabled>Select Branch</option>
                     <option value="2">CSE</option>
                     <option value="3">CSE(Honors)</option>
                     <option value="4">EEE</option>
@@ -106,6 +253,7 @@ export default function Registrationform() {
                     <option value="6">Mechanical</option>
                     <option value="7">BBA</option>
                   </select>
+                 ( Branch)
   
 </div>
 
@@ -113,14 +261,15 @@ export default function Registrationform() {
 <div className="col-md-6 mb-4 pb-2">
 
 <div className="form-outline">
-<input  type="text" className="form-control form-control-lg"  value="Student" readonly/>
+<input  type="text" className="form-control form-control-lg stu" name="mand" value="Student" readonly/>
 </div>
 
               </div>
               </div>
 
               <div>
-              <button className="form-control form-control-lg reg" onClick={()=>navigate('regis')}>Register</button>
+              {/* onClick={()=>navigate('regis')} */}
+              <input type="submit" className="form-control form-control-lg reg" value="Register"/>
                 
               </div>
           
